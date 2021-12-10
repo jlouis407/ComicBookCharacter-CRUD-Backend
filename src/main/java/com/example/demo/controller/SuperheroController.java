a318427764fc2cc5fb19005bb692533ad182b904
Josiah Louis <josiah.israel.louis@gmail.com>
2021-12-10 14:47:40 -0500

    Update SuperheroController

--- src/main/java/com/example/demo/controller/SuperheroController.java
+++ src/main/java/com/example/demo/controller/SuperheroController.java
@@ -3,13 +3,17 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Superhero;
 import com.example.demo.repository.SuperheroRepository;
 
@@ -31,5 +35,26 @@ public class SuperheroController {
 		return superheroRepository.save(superhero);
 	}
 	
-
+	@GetMapping("/superheroes/{id}")
+	public ResponseEntity<Superhero> getSuperheroById(@PathVariable Long id) {
+		Superhero superhero = superheroRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Superhero with id: " + id + " does not exist."));
+		return ResponseEntity.ok(superhero);
+	}
+	
+	@PutMapping("/superheroes/{id}")
+	public ResponseEntity<Superhero> updateSuperhero(@PathVariable Long id, @RequestBody Superhero superheroDetails){
+		Superhero superhero = superheroRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Superhero with id: " + id + " does not exist."));
+		
+		superhero.setName(superheroDetails.getName());
+		superhero.setBrand(superheroDetails.getBrand());
+		superhero.setYear(superheroDetails.getYear());
+		superhero.setStatus(superheroDetails.getStatus());
+		
+		Superhero updatedSuperhero = superheroRepository.save(superhero);
+		return ResponseEntity.ok(updatedSuperhero);
+		
+	}
 }
+	
